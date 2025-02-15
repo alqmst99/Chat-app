55607a5849fecbbb09451e244f116a4c7479ccb4
Nahuel N. Pierini <nahuel.pierini90@hotmail.com>
2025-02-15 16:11:04 -0300

    create frame, Resize undecorated. and bar press, redimention, drag

---
+++ src/chat/realtime/app/ChatRealtimeApp.java
@@ -0,0 +1,15 @@
+
+package chat.realtime.app;
+
+/**
+ *
+ * @author Nahuel N. Pierini
+ */
+public class ChatRealtimeApp {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
